7a5f19446dc4efeaafa50d2042c9452c40f7652b
adriafernandezarans <adrifernandevs@gmail.com>
2024-01-03 14:27:37 +0100

    [feat] Implemented docker

--- src/main/kotlin/com/madteam/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/madteam/repository/DatabaseFactory.kt
@@ -2,13 +2,13 @@ package com.madteam.repository
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
 
-private const val JDBC_DRIVER_ENVIRONMENT = "JDBC_DRIVER"
-private const val JDBC_DB_URL_ENVIRONMENT = "JDBC_DATABASE_URL"
+private const val JDBC_DRIVER = "org.postgresql.Driver"
 private const val MAXIMUM_POOL_SIZE = 3
 
 object DatabaseFactory {
@@ -19,9 +19,12 @@ object DatabaseFactory {
 
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
+        val environment = ApplicationConfig(null)
         with(config){
-            driverClassName = System.getenv(JDBC_DRIVER_ENVIRONMENT)
-            jdbcUrl = System.getenv(JDBC_DB_URL_ENVIRONMENT)
+            driverClassName = JDBC_DRIVER
+            jdbcUrl = environment.property("storage.jdbc_url").getString()
+            username = environment.property("storage.username").getString()
+            password = environment.property("storage.password").getString()
             maximumPoolSize = MAXIMUM_POOL_SIZE
             isAutoCommit = false
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
