6d1e58e820ce2b7bd1d4665c7c0b1fe982f565cc
adriafernandezarans <adrifernandevs@gmail.com>
2024-01-06 17:18:26 +0100

    [feat] WIP: Doing changes to swagger

--- src/main/kotlin/com/madteam/plugins/Swagger.kt
+++ src/main/kotlin/com/madteam/plugins/Swagger.kt
@@ -2,13 +2,16 @@ package com.madteam.plugins
 
 import io.github.smiley4.ktorswaggerui.SwaggerUI
 import io.ktor.server.application.*
-import io.ktor.server.application.*
+import io.ktor.server.routing.*
 
 fun Application.configureSwagger(){
     install(SwaggerUI){
         swagger {
             swaggerUrl = "swagger"
             forwardRoot = true
+        }
+        routing {
+            
         }
         info {
             title = "Split back API"
