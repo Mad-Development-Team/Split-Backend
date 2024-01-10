fb722c6d48d595d0585331c605085613e284c2d5
adriafernandezarans <adrifernandevs@gmail.com>
2024-01-10 18:58:05 +0100

    [feat] Implemented getUserInfo

--- src/main/kotlin/com/madteam/routes/UserRoutes.kt
+++ src/main/kotlin/com/madteam/routes/UserRoutes.kt
@@ -1,5 +1,6 @@
 package com.madteam.routes
 
+import com.madteam.data.model.User
 import com.madteam.repository.UserRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -8,24 +9,35 @@ import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.getUserInfo(){
+fun Route.getUserInfo() {
     authenticate {
-     get("userInfo") {
-         val principal = call.principal<JWTPrincipal>()
-         val userId = principal?.getClaim("userId", String::class)
+        get("getUserInfo") {
+            val principal = call.principal<JWTPrincipal>()
+            val userId = principal?.getClaim("userId", String::class)
 
-         if (userId != null) {
+            if (userId != null) {
 
-             val user = UserRepository().findUserById(userId.toInt())
+                val user = UserRepository().findUserById(userId.toInt())
 
-             if (user != null) {
-                 call.respond(HttpStatusCode.OK, user)
-             } else {
-                 call.respond(HttpStatusCode.NotFound, "User not found")
-             }
-         } else {
-             call.respond(HttpStatusCode.Unauthorized, "Unauthorized")
-         }
-     }
+                if (user != null) {
+                    call.respond(
+                        HttpStatusCode.OK,
+                        User(
+                            id = user.id,
+                            name = user.name,
+                            email = user.email,
+                            passwordHash = "",
+                            profileImage = user.profileImage ?: "",
+                            createdDate = user.createdDate,
+                            passwordSalt = ""
+                        )
+                    )
+                } else {
+                    call.respond(HttpStatusCode.NotFound, "User not found")
+                }
+            } else {
+                call.respond(HttpStatusCode.Unauthorized, "Unauthorized")
+            }
+        }
     }
 }
